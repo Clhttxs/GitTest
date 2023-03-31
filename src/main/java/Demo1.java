2a3916132d35ed1f39c617c9dc9d7ef602e5ba9f
程橹豪 <129243242+Clhttxs@users.noreply.github.com>
2023-03-31 16:35:42 +0800

    Update Demo1.java

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -3,5 +3,6 @@ public class Demo1 {
         System.out.println("hello Git 111111");
         System.out.println("hello Git 222222");
         System.out.println("hello Git master");
+        System.out.println("hello GitHub");
     }
 }
